acd92d616e35cd0c516e25278c01791e08303806
marisiman <email_dari_marisiman@example.com>
2024-07-01 10:17:00 +0700

    completed experiences page

--- src/common/ExperienceCard.jsx
+++ src/common/ExperienceCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function ExperienceCard({ src, link, h3, p }) {
+function ExperienceCard({ src, link, h3, h4, p }) {
   return (
     <a href={link} target="_blank">
       <img className="hover" src={src} alt={`${h3} logo`} />
       <h3>{h3}</h3>
+      <h4>{h4}</h4>
       <p>{p}</p>
     </a>
   )
--- src/sections/Experiences/Experiences.jsx
+++ src/sections/Experiences/Experiences.jsx
@@ -1,7 +1,9 @@
 import styles from './ExperiencesStyles.module.css';
-import projectOne from '../../assets/number1.png';
-import projectTwo from '../../assets/number2.png';
-import projectThree from '../../assets/number3.png';
+import projectOne from '../../assets/kebon-agung.png';
+import projectTwo from '../../assets/meiwa-mold.png';
+import projectThree from '../../assets/mitsuba.png';
+import projectFour from '../../assets/gs-battery.png';
+
 
 import ExperienceCard from '../../common/ExperienceCard';
 
@@ -11,25 +13,37 @@ function Experiences() {
         <h1 className='sectionTitle'>Experiences</h1>
         <div className={styles.projectsContainer}>
             <ExperienceCard
-            src={projectOne}
-            link="https://github.com/imanmaris99/developprogramdonasi_BE2024"
-            h3="Program Donasi BE Dev."
-            p="About it"
-            />
-            
-            <ExperienceCard
-            src={projectTwo}
-            link="https://github.com/imanmaris99/developprogramdonasi_BE2024"
-            h3="Herbal Shop FE Dev."
-            p="About it"
+              src={projectOne}
+              link="http://www.pgtrangkil.com/"
+              h3="PT. Kebon Agung-Trangkil Plant"
+              h4="Boiler Operator"
+              p="April 2023 - May 2023"
             />
 
             <ExperienceCard
-            src={projectThree}
-            link="https://github.com/imanmaris99/surface-treatment-Iman-maris"
-            h3="Surface Treatment FE Dev."
-            p="About it"
+              src={projectTwo}
+              link="https://meiwa-m.co.id/"
+              h3="PT. Meiwa Mold Indonesia"
+              h4="Product Engineering"
+              p="January 2019 - January 2023"
             />
+
+            <ExperienceCard
+              src={projectThree}
+              link="https://github.com/imanmaris99/developprogramdonasi_BE2024"
+              h3="PT. Mitsuba Indonesia"
+              h4="Product Quality Control"
+              p="Agustus 2017 - September 2018"
+            />
+
+            <ExperienceCard
+              src={projectFour}
+              link="https://www.gs.astra.co.id/"
+              h3="PT. GS Battery-Semarang Plant"
+              h4="Assemblly"
+              p="January 2016 - May 2016"
+            />
+            
         </div>
 
     </section>
